d6460d3caa65d5433fd41bd94c76f6fac13aa86d
mizuki <75738412+mizuki51@users.noreply.github.com>
2021-12-09 17:55:42 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = function () {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
